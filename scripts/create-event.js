7ea0bdeec9022741e3d79e30f9badf9103f0a755
Asisipho <mjojeliasisipho@gmail.com>
2025-05-08 20:50:55 +0200

    Update create-event styles and JavaScript functionality

--- scripts/create-event.js
+++ scripts/create-event.js
@@ -7,3 +7,13 @@ function getEventsFromStorage() {
 function saveEventsToStorage(events) {
   localStorage.setItem('events', JSON.stringify(events));
 }
+function showSuccessful(event) {
+  event.preventDefault(); 
+  document.getElementById("overlay").style.display = "flex";
+  document.body.style.overflow = 'hidden';
+}
+
+function closeCustomAlert() {
+  document.getElementById("overlay").style.display = "none";
+  document.body.style.overflow = '';
+}
